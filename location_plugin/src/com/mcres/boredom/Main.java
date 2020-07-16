6e055fd834860dab1636bb09cce9224b5da25c8b
kritinsinghal <kritinsinghal@gmail.com>
2020-07-16 18:23:09 +0530

    plugin updates

--- location_plugin/src/com/mcres/boredom/Main.java
+++ location_plugin/src/com/mcres/boredom/Main.java
@@ -1,14 +1,17 @@
 package com.mcres.boredom;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.WorldBorder;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.json.simple.JSONArray;
@@ -26,10 +29,11 @@ import java.util.TimerTask;
 
 public class Main extends JavaPlugin implements Listener {
 
-    double x, y, z;
+    double x, y, z, yaw;
+    String item = "", block="", blockState="", worldState="";
     JSONArray location = new JSONArray();
     Player player;
-    int numPlayers = 0;
+    int numPlayers = 0, sprintFlag = 0;
     String fileName;
 
     @Override
@@ -71,27 +75,39 @@ public class Main extends JavaPlugin implements Listener {
         timerData();
     }
 
+
+
     public void init() throws IOException {
         fileName = "data/" + player.getName() + (new Timestamp(System.currentTimeMillis())) + ".json";
         FileWriter writer = new FileWriter(fileName);
         JSONObject ob = new JSONObject();
-        ob.put("location", new JSONArray());
+//        TimeChangeEvent();
+        ob.put("events", new JSONArray());
         writer.write(ob.toJSONString());
         writer.close();
     }
 
+    public void TimeChangeEvent() {
+        if (day()) {
+            worldState = "day";
+        } else {
+            worldState = "night";
+        }
+    }
+
     public void saveData() {
         try {
+//            TimeChangeEvent();
             FileReader reader = new FileReader(fileName);
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
-            JSONArray loc = (JSONArray) jsonObject.get("location");
+            JSONArray loc = (JSONArray) jsonObject.get("events");
             FileWriter writer = new FileWriter(fileName);
             for (Object obj: location) {
                 loc.add(obj);
             }
             location.clear();
-            jsonObject.put("location", loc);
+            jsonObject.put("events", loc);
             writer.write(jsonObject.toJSONString());
             writer.close();
         } catch (IOException | ParseException e) {
@@ -111,40 +127,120 @@ public class Main extends JavaPlugin implements Listener {
         timer.schedule(minuteTask, 0l, 1000*120);
     }
 
-
+    public boolean day() {
+        Server server = getServer();
+        String WorldName = player.getServer().getName();
+        long time = server.getWorld(WorldName).getTime();
+        return time < 12300 || time > 23850;
+    }
 
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
         double getX = e.getTo().getBlockX();
         double getY = e.getTo().getBlockY();
         double getZ = e.getTo().getBlockZ();
+        double yawL = e.getTo().getYaw();
         double dist = Math.sqrt(Math.pow(x - getX, 2) + Math.pow(y - getY, 2) + Math.pow(z - getZ, 2));
         getLogger().info(String.valueOf(dist));
-        if (dist >= 1) {
+        if (Math.abs(yawL - yaw) > 45) {
             x = getX;
             y = getY;
             z = getZ;
-            updateObjects(0);
+            yaw = yawL;
+            updateObjects("PlayerMoveEvent");
+        } else if (dist >= 1) {
+            x = getX;
+            y = getY;
+            z = getZ;
+            yaw = yawL;
+            updateObjects("PlayerMoveEvent");
         }
     }
 
-    public void updateObjects(int block) {
+    @EventHandler
+    public void onPlayerAnimation(final PlayerAnimationEvent event) {
+        item = event.getAnimationType().toString();
+        updateObjects("PlayerAnimationEvent");
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block b = event.getBlock();
+        block = b.getBlockData().getAsString();
+        blockState = b.getState().toString();
+        updateObjects("BlockBreakEvent");
+    }
+
+    @EventHandler
+    public void checkBlockDamage(final BlockDamageEvent event) {
+        blockState = event.getBlock().getState().toString();
+        block = event.getBlock().toString();
+        updateObjects("BlockDamageEvent");
+    }
+
+    public void reinit() {
+        item = "";
+        block = "";
+        blockState = "";
+    }
+
+    public void updateObjects(String event) {
         JSONObject coords = new JSONObject();
         JSONArray coordsarr = new JSONArray();
+        Timestamp t = new Timestamp(System.currentTimeMillis());
+        String time = t.toString();
         coordsarr.add(x);
         coordsarr.add(y);
         coordsarr.add(z);
+        coords.put("event", event);
         coords.put("coords", coordsarr);
-        coords.put("timestamp", new Timestamp(System.currentTimeMillis()));
-//        coords.put("view_angle", viewVector);
-//        if (block == 0) {
-//            coords.put("block", null);
-//        } else {
-//            coords.put("block", blockActivity);
-//        }
+//        coords.put("time", worldState);
+        coords.put("yaw", yaw);
+        coords.put("timestamp", time);
+        if (event.equals("PlayerAnimationEvent")) {
+            coords.put("animation", item);
+        }
+        if (event.equals("PlayerItemHeldEvent")) {
+            coords.put("itemHeld", item);
+        }
+        if (event.equals("BlockPlaceEvent")) {
+            coords.put("block", block);
+        }
+        if ((event.equals("BlockDamageEvent")) || (event.equals("BlockBreakEvent"))) {
+            coords.put("block", block);
+            coords.put("blockState", blockState);
+        }
+        reinit();
         location.add(coords);
     }
 
+    @EventHandler
+    public void onMonitorBlockPlace(BlockPlaceEvent event) {
+        block = event.getBlock().toString();
+        updateObjects("BlockPlaceEvent");
+    }
+
+    @EventHandler
+    public void onPlayerToggleSprint(final PlayerToggleSprintEvent event) {
+        if (event.isSprinting() && sprintFlag == 0) {
+            sprintFlag = 1;
+            updateObjects("PlayerSprintingEvent");
+        }
+        if (!event.isSprinting() && sprintFlag == 1) {
+            sprintFlag = 0;
+            updateObjects("PlayerSlowingEvent");
+        }
+    }
+
+    @EventHandler
+    public void onItemHeld(PlayerItemHeldEvent event) {
+        ItemStack stack = event.getPlayer().getItemOnCursor();
+        if (stack == null) {
+            return;
+        }
+        item = stack.toString();
+        updateObjects("PlayerItemHeldEvent");
+    }
 
     @Override
     public void onDisable() {
